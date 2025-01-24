79576724e74a996427dc2db8061003c0deb15ffd
Mathias Rendon <mathiassaid7@gmail.com>
2025-01-24 16:47:35 -0500

    Feat: Add header

---
+++ src/components/Header.jsx
@@ -0,0 +1,20 @@
+import '../App.css';
+import githubIcon from '../../public/github.svg';
+
+function Header() {
+
+    const handleGitHub = () => {
+        window.open("https://github.com/JunLovin", "_blank");
+    }
+
+    return (
+        <header>
+            <nav>
+                <a href="/"><h1 className="header">cvdotcom</h1></a>
+                <img src={githubIcon} alt="GitHub" title="Click here to go to my GitHub" onClick={handleGitHub} />
+            </nav>
+        </header>
+    );
+}
+
+export default Header;
