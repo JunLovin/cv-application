d7000a43625e6f216658ea3bfe7b9e3301604fa5
Mathias Rendon <mathiassaid7@gmail.com>
2025-01-24 21:30:29 -0500

    feat: add span X like button

--- src/components/CardTemplate.jsx
+++ src/components/CardTemplate.jsx
@@ -4,6 +4,7 @@ function CardTemplate(props) {
     return (
         <>
             <div className="card">
+                <span className="card-close" onClick={props.handleClose}>X</span>
                 <div className="card-header">
                     <h2>{props.title}</h2>
                 </div>
